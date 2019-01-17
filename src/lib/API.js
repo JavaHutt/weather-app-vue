88d4d9ddf0cf01033dbcd276d3628d0c8d037c6a
unknown <alxmer111@gmail.com>
2019-01-17 20:48:44 +0400

    api request working

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,4 +1,6 @@
-const API_URL = 'https://api.darksky.net/forecast/7102c032dfc82cbe484875a41e5ac1a5/37.8267,-122.4233?lang=ru&units=si';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+const API_KEY = '7102c032dfc82cbe484875a41e5ac1a5';
+const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233?lang=ru&units=si`;
 
 function getForecast() {
     fetch(API_URL)
