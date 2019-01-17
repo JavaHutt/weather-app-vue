62c8b66830ab4196a9f1cfce8bd46da7167217de
unknown <alxmer111@gmail.com>
2019-01-17 22:16:25 +0400

    trying to use axios, seems to work

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,13 +1,11 @@
+import axios from 'axios';
+
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const API_KEY = '7102c032dfc82cbe484875a41e5ac1a5';
 const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233?lang=ru&units=si`;
 
 function getForecast() {
-    fetch(API_URL)
-        .then(response => response.json())
-        .then((result) => {
-            console.log(result);
-        });
+    return axios.get(API_URL);
 }
 
 export default {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-  </div>
+    <div class="home">
+        <div class="row">
+            <div class="col-6">
+                <div class="card text-white bg-secondary mb-3">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Secondary card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title
+                            and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card text-white bg-secondary mb-3">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Secondary card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title
+                            and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <pre>{{forecast}}</pre>
+    </div>
 </template>
 
 <script>
 import API from '@/lib/API';
+
 export default {
-  name: 'home',
-  mounted() {
-    API.getForecast();
-  }
+    name: 'home',
+    data() {
+        return {
+            forecast: {},
+        };
+    },
+    mounted() {
+        API.getForecast().then((result) => {
+            this.forecast = result;
+        });
+    },
 };
 </script>
