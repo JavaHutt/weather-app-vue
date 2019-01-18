ddd19218e32875f97739363e307154ce739b2d11
unknown <alxmer111@gmail.com>
2019-01-18 04:06:32 +0400

    vue filters explored

--- src/lib/API.js
+++ src/lib/API.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 const API_KEY = '7102c032dfc82cbe484875a41e5ac1a5';
-const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233?lang=ru&units=si`;
+const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233?&exclude=[minutely,hourly,daily]&lang=ru&units=si`;
 
-function getForecast() {
+function getDarkSkyForecast() {
     return axios.get(API_URL);
 }
 
 export default {
-    getForecast,
+    getDarkSkyForecast,
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,18 @@
     <div class="home">
         <div class="row">
             <div class="col-6">
-                <div class="card text-white bg-secondary mb-3">
-                    <div class="card-header">Header</div>
+                <div class="card text-white bg-secondary mb-3 text-center"
+                 v-if="darksky.currently">
+                    <div class="card-header">Darksky weather</div>
                     <div class="card-body">
-                        <h4 class="card-title">Secondary card title</h4>
-                        <p class="card-text">Some quick example text to build on the card title
-                            and make up the bulk of the card's content.</p>
+                        <h4 class="card-title">
+                            {{darksky.currently.summary}}</h4>
+                        <div class="card-text">
+                            {{darksky.currently.icon}}
+                        </div>
+                        <div class="card-text">
+                            {{darksky.currently.temperature | temperaturedecimal}} &deg;C
+                        </div>
                     </div>
                 </div>
             </div>
@@ -22,7 +28,7 @@
                 </div>
             </div>
         </div>
-        <pre>{{forecast}}</pre>
+        <pre>{{darksky}}</pre>
     </div>
 </template>
 
@@ -33,12 +39,17 @@ export default {
     name: 'home',
     data() {
         return {
-            forecast: {},
+            darksky: {},
         };
     },
+    filters: {
+        temperaturedecimal(value) {
+            return value.toFixed();
+        },
+    },
     mounted() {
-        API.getForecast().then((result) => {
-            this.forecast = result;
+        API.getDarkSkyForecast().then((result) => {
+            this.darksky = result.data;
         });
     },
 };
