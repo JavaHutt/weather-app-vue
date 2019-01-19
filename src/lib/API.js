0883cc2dee867d90d2f39191908485d6125a9bef
unknown <alxmer111@gmail.com>
2019-01-19 04:05:09 +0400

    TYPESCRIPT IS GETTING ON MY NERVES

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
-const API_KEY = '7102c032dfc82cbe484875a41e5ac1a5';
-const API_URL = `${proxy}https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233?&exclude=[minutely,hourly,daily]&lang=ru&units=si`;
+const API_KEY_DARKSKY = '7102c032dfc82cbe484875a41e5ac1a5';
+const API_KEY_OPEN = '9ba6e5ed9610d49dcea7d39e4a9b4943';
+const API_KEY_WEATHERBIT = '2bdda14f1a0e4c2b8e272373cac86afe';
+const API_DARKSKY = `${proxy}https://api.darksky.net/forecast/${API_KEY_DARKSKY}/37.8267,-122.4233?&exclude=[minutely,hourly,daily]&lang=ru&units=si`;
+const API_OPEN = `https://api.openweathermap.org/data/2.5/weather?lat=37.8267&lon=-122.4233&mode=json&appid=${API_KEY_OPEN}&units=metric&lang=ru`;
+const API_WEATHERBIT = `https://api.weatherbit.io/v2.0/current?&lat=37.8267&lon=-122.4233&key=${API_KEY_WEATHERBIT}&lang=ru`;
 
 function getDarkSkyForecast() {
-    return axios.get(API_URL);
+    return axios.get(API_DARKSKY);
+}
+function getOpenWeatherForecast() {
+    return axios.get(API_OPEN);
+}
+function getWeatherbitForecast() {
+    return axios.get(API_WEATHERBIT);
 }
 
 export default {
     getDarkSkyForecast,
+    getOpenWeatherForecast,
+    getWeatherbitForecast,
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
     <div class="home">
         <div class="row">
-            <div class="col-6">
+            <div class="col-4">
                 <div class="card text-white bg-secondary mb-3 text-center"
                  v-if="darksky.currently">
                     <div class="card-header">Darksky weather</div>
                     <div class="card-body">
-                        <h4 class="card-title">
+                        <h4 class="card-title summary">
                             {{darksky.currently.summary}}</h4>
                         <div class="card-text">
-                            {{darksky.currently.icon}}
+                            {{icons[darksky.currently.icon]}}
+                        </div>
+                        <div class="card-text">
                         </div>
                         <div class="card-text">
                             {{darksky.currently.temperature | temperaturedecimal}} &deg;C
@@ -17,18 +19,26 @@
                     </div>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="card text-white bg-secondary mb-3">
-                    <div class="card-header">Header</div>
+            <div class="col-4">
+                <div class="card text-white bg-secondary mb-3 text-center"
+                 v-if="openweather">
+                    <div class="card-header">OpenWeatherMap weather</div>
                     <div class="card-body">
-                        <h4 class="card-title">Secondary card title</h4>
-                        <p class="card-text">Some quick example text to build on the card title
-                            and make up the bulk of the card's content.</p>
+                        <h4 class="card-title summary">
+                            {{openweather.weather[0].description}}</h4>
+                        <div class="card-text">
+                            {{openweather.weather[0].icon}}
+                        </div>
+                        <div class="card-text">
+                        </div>
+                        <div class="card-text">
+                            {{openweather.main.temp | temperaturedecimal}} &deg;C
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <pre>{{darksky}}</pre>
+        <pre>{{openweather}}</pre>
     </div>
 </template>
 
@@ -40,17 +50,51 @@ export default {
     data() {
         return {
             darksky: {},
+            openweather: {},
+            weatherbit: {},
+            icons: {
+                'clear-day': '<i class="wi wi-day-sunny"></i>',
+                'clear-night': '<i class="wi wi-night-clear"></i>',
+                rain: '<i class="wi wi-rain"></i>',
+                snow: '<i class="wi wi-snow"></i>',
+                sleet: '<i class="wi wi-sleet"></i>',
+                wind: '<i class="wi wi-strong-wind"></i>',
+                fog: '<i class="wi wi-fog"></i>',
+                cloudy: '<i class="wi wi-cloudy"></i>',
+                'partly-cloudy-day': '<i class="wi day-cloudy-high"></i>',
+                'partly-cloudy-night': '<i class="wi night-alt-partly-cloudy"></i>',
+            },
         };
-    },
-    filters: {
-        temperaturedecimal(value) {
-            return value.toFixed();
-        },
     },
     mounted() {
         API.getDarkSkyForecast().then((result) => {
             this.darksky = result.data;
         });
+        API.getOpenWeatherForecast().then((result) => {
+            this.openweather = result.data;
+        });
+        API.getWeatherbitForecast().then((result) => {
+            this.weatherbit = result.data;
+        });
+    },
+    filters: {
+        temperaturedecimal(value) {
+            return value.toFixed(1);
+        },
     },
 };
 </script>
+
+<style>
+@font-face {
+    font-family: 'WeatherIcons';
+    src: url('../font/weathericons-regular-webfont.eot');
+    src: url('../font/weathericons-regular-webfont.woff2') format('woff2'),
+        url('../font/weathericons-regular-webfont.woff') format('woff'),
+        url('../font/weathericons-regular-webfont.ttf')  format('truetype'),
+        url('../font/weathericons-regular-webfont.svg') format('svg');
+}
+.summary {
+    font-size: 40px;
+}
+</style>
